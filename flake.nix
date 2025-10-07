20b33459eadef11836d57c86954dfd1556d5eda6
emzywastaken <amiamemetoo@gmail.com>
2025-10-07 17:07:15 +0100

    unset `PKG_CONFIG_PATH` env var

--- flake.nix
+++ flake.nix
@@ -25,8 +25,6 @@
       ];
 
       shellHook = ''
-        export PKG_CONFIG_PATH="${pkgs.xorg.libX11.dev}/lib/pkgconfig:${pkgs.xorg.libXft.dev}/lib/pkgconfig:${pkgs.xorg.libXrender.dev}/lib/pkgconfig:${pkgs.freetype.dev}/lib/pkgconfig:${pkgs.fontconfig.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
-
         export PS1="(oxwm-dev) $PS1"
       '';
 
